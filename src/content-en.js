c3143e3a026dee0677dac175bd9f2a3ace33d848
Francisco Lucena <franciscolucena90@gmail.com>
2023-09-29 21:01:15 -0300

    initial commit v6

---
+++ src/content-en.js
@@ -0,0 +1,67 @@
+export const companyContent = {
+    header: 'Company',
+    aboutUs: 'About Us',
+    blog: 'Blog',
+    careers: 'Careers',
+    contactUs: 'Contact Us',
+  };
+  
+  export const supportContent = {
+    header: 'Support',
+    helpCenter: 'Help Center',
+    safetyCenter: 'Safety Center',
+    communityGuidelines: 'Community Guidelines',
+  };
+
+  export const legalContent = {
+    header: 'Legal',
+    cookiesPolicy: 'Cookies Policy',
+    privacyPolicy: 'Privacy Policy',
+    termsOfService: 'Terms of Service',
+    lawEnforcement: 'Law Enforcement',
+  };
+
+  export const allRightsReserved = {
+    text: 'All rights reserved.',
+  };
+  
+  export const navbar = {
+    buttonText: 'Join!',
+    login: 'Sign In',
+    search: 'Browse',
+    signOut: 'Sign Out',
+    favorites: 'My Favorites'
+  };
+
+  export const newsletter = {
+    title: 'SUBSCRIBE',
+    description: 'Subscribe to our newsletter & stay up to date!',
+    placeholder: 'Your email',
+    buttonText: 'Subscribe'
+  }
+
+  export const login = {
+    headingText: 'Log In',
+    emailLabel: 'Email',
+    passwordLabel: 'Password',
+    loginButtonLabel: 'LOG IN',
+    signUpLinkText: 'Don\'t have an account? ',
+    signUpLink: 'Sign Up'
+  }
+  
+  export const signup = {
+    headingText: 'Sign Up',
+    subheadingText: 'to enjoy all the benefits!',
+    firstNameLabel: 'First Name',
+    lastNameLabel: 'Last Name',
+    emailLabel: 'Email Address',
+    passwordLabel: 'Password',
+    signupButtonLabel: 'Sign Up',
+    loginLinkText: 'Already a user? ',
+    loginLink: 'Log In'
+  }
+  
+  export const pagination = {
+    previousPage: "Previous",
+    nextPage: "Next"
+  }
---
+++ src/content-es.js
@@ -0,0 +1,67 @@
+export const companyContent = {
+  header: 'Compañía',
+  aboutUs: 'Acerca de Nosotros',
+  blog: 'Blog',
+  careers: 'Carreras',
+  contactUs: 'Contáctanos',
+};
+
+export const supportContent = {
+  header: 'Soporte',
+  helpCenter: 'Centro de Ayuda',
+  safetyCenter: 'Centro de Seguridad',
+  communityGuidelines: 'Pautas de la Comunidad',
+};
+
+export const legalContent = {
+  header: 'Legal',
+  cookiesPolicy: 'Política de Cookies',
+  privacyPolicy: 'Política de Privacidad',
+  termsOfService: 'Términos de Servicio',
+  lawEnforcement: 'Cumplimiento Legal',
+};
+
+export const allRightsReserved = {
+  text: 'Todos los derechos reservados.',
+};
+
+export const navbar = {
+  buttonText: 'Ser parte',
+  login: 'Conectarme',
+  search: 'Explorar',
+  signOut: 'Desconectarme',
+  favorites: 'Mis favoritos'
+};
+
+export const newsletter = {
+  title: 'SUSCRIBIRME',
+  description: '¡Suscríbete a nuestro boletín y mantente actualizado!',
+  placeholder: 'Tu email',
+  buttonText: 'Suscribirme'
+}
+
+export const login = {
+  headingText: 'Iniciar sesión',
+  emailLabel: 'Email',
+  passwordLabel: 'Contraseña',
+  loginButtonLabel: 'INICIAR SESIÓN',
+  signUpLinkText: 'Sin cuenta? ',
+  signUpLink: 'Registrarme'
+}
+
+export const signup = {
+  headingText: 'Registrarme',
+  subheadingText: 'para obtener todos los beneficios!',
+  firstNameLabel: 'Nombre',
+  lastNameLabel: 'Apellido',
+  emailLabel: 'Dirección de email',
+  passwordLabel: 'Contraseña',
+  signupButtonLabel: 'Registrarme',
+  loginLinkText: 'Ya eres usuario? ',
+  loginLink: 'Conectarme'
+}
+
+export const pagination = {
+  previousPage: "Anterior",
+  nextPage: "Siguiente"
+}
